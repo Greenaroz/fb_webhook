eab84036220d2084422e2362ac61b837a547beb3
Aromal Sivadasan <arrow@Aromals-MacBook-Air.local>
2017-10-27 22:17:06 +0530

    poatback and attachments

--- index.js
+++ index.js
@@ -91,7 +91,8 @@ function handleMessage(sender_psid, received_message) {
       // Create the payload for a basic text message
       response = {
         "text": `You sent the message: "${received_message.text}". Now send me an image!`
-      } else if (received_message.attachments) {
+      }
+    } else if (received_message.attachments) {
 
         // Get the URL of the message attachment
          let attachment_url = received_message.attachments[0].payload.url;
