faef123d376d7dfdb22c5a93b231f7530676f596
Aromal Sivadasan <arrow@Aromals-MacBook-Air.local>
2017-11-01 21:36:59 +0530

    testing reply

--- app.js
+++ app.js
@@ -122,6 +122,9 @@ function replyToComment(post_comment_id, response){
     "message": response
   }
 
+  console.log('POST COMMENT ID = ' + post_comment_id);
+  console.log('RESPONSE = ' + JSON.stringify(response));
+
   let uri = "https://graph.facebook.com/v2.10/me/"+ post_comment_id + "/private_replies";
   console.log('URI = ' + uri);
 
