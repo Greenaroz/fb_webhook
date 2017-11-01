b6d8b2b1a5b2bb9ed81fb7c89ab25678d9e81ba8
Aromal Sivadasan <arrow@Aromals-MacBook-Air.local>
2017-11-01 22:20:12 +0530

    testing reply

--- app.js
+++ app.js
@@ -125,13 +125,16 @@ function replyToComment(post_comment_id, response){
   console.log('POST COMMENT ID = ' + post_comment_id);
   console.log('RESPONSE = ' + JSON.stringify(response));
 
-  let uri = "https://graph.facebook.com/v2.10/me/" + post_comment_id+ "/private_replies";
+  let uri = "https://graph.facebook.com/v2.10/me/private_replies";
   console.log('URI = ' + uri);
 
     // Send the HTTP request to the Messenger Platform
   request({
     "uri": uri,
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "qs": {
+            "access_token": PAGE_ACCESS_TOKEN,
+            "comment_id" : post_comment_id  
+          },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
